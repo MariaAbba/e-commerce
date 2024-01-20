09e7f0cb92f2399c871b76d566a5a500737caa16
Mary <gretchen7887@gmail.com>
2024-01-20 21:35:10 +0400

    Upd

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+import Navbar from './Components/Navbar/Navbar'
+
+function App() {
+
+
+  return (
+<div>
+  <Navbar/>
+</div>
+  )
+}
+
+export default App
---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import './Navbar.css'
+import logo from '../Assets/logo.png'
+import cart_icon from '../Assets/cart_icon.png'
+
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      <div className="nav-logo">
+        <img src={logo} alt="Logo" />
+        <p>Shopper</p>
+      </div>
+      <ul className="nav-menu">
+        <li>Shop <hr/></li>
+        <li>Men</li>
+        <li>Women</li>
+        <li>Kids</li>
+      </ul>
+      <div className="nav-login-cart">
+        <button>Login</button>
+        <img src={cart_icon} alt="Cart" />
+        <div className="nav-cart-count">0</div>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
