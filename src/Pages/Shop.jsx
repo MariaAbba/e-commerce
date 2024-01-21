050f1b7bd7b21b001dc2e084886d8458838c5977
Mary <gretchen7887@gmail.com>
2024-01-21 13:26:32 +0400

    Upd

--- src/Pages/Shop.jsx
+++ src/Pages/Shop.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import Hero from '../Components/Hero/Hero'
 import Popular from '../Components/Popular/Popular'
+import Offers from '../Components/Offers/Offers'
+import NewCollections from '../Components/NewCollections/NewCollections'
 
 const Shop = () => {
   return (
     <div>
       <Hero/>
       <Popular/>
+     <Offers/>
+     <NewCollections/>
     </div>
   )
 }
